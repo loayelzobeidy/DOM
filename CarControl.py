0e7f178aebc4df761c6326dd1835ff714f592b2c
loayelzobeidy <loayelzobeidy@gmail.com>
2018-05-05 19:03:50 +0000

    CarContoller modified

--- CarControl.py
+++ CarControl.py
@@ -43,19 +43,18 @@ def getch():
 
 GPIO.setmode(GPIO.BOARD)
 import time
-right_wheel_red=3
-right_wheel_brown=5
-left_wheel_yellow =8
-left_wheel_orange=10
+right_wheel_red=11
+right_wheel_brown=13
+left_wheel_yellow =23
+#left_wheel_orange=
+left_wheel_orange=24
 #setup led(pin 8) as output pin
 GPIO.setup(right_wheel_red, GPIO.OUT,initial=0)
+#GPIO.setup(33, GPIO.OUT,initial=1)
+#GPIO.setup(31, GPIO.OUT,initial=1)
 GPIO.setup(left_wheel_yellow, GPIO.OUT,initial=0)
 GPIO.setup(right_wheel_brown, GPIO.OUT,initial=0)
 GPIO.setup(left_wheel_orange, GPIO.OUT,initial=0)
-trig_left = 11
-trig_right=12
-GPIO.setup(trig_right, GPIO.IN)
-GPIO.setup(trig_left, GPIO.IN)
 try:
     print('ready')
 except KeyboardInterrupt:
@@ -63,16 +62,16 @@ except KeyboardInterrupt:
     GPIO.cleanup()
     print("Exiting...")
 while(True):
-	right_signal=GPIO.input(trig_right)
-	left_signal= GPIO.input(trig_left)
-	if(left_signal==1):
-	    Turn_right(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)
-	if(right_signal==1):
-	    Turn_left(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)
-        if(right_signal==1 and left_signal==1):
-		Forward(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)
-        if(right_signal==0 and left_signal==0):
-            Forward(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)            
+#	right_signal=GPIO.input(trig_right)
+#	left_signal= GPIO.input(trig_left)
+#	if(left_signal==1):
+  #          Turn_right(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)
+#	if(right_signal==1):
+ #           Turn_left(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)
+   #     if(right_signal==1 and left_signal==1):
+  #          Forward(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)
+ #       if(right_signal==0 and left_signal==0):
+#            Forward(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)            
 	input = getch()
 	if(input=='a'):
             Turn_left(right_wheel_red,right_wheel_brown,left_wheel_yellow,left_wheel_orange)
